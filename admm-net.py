bb0aeef4b9c5d4dbc45f0d8f2f551ead365b40d0
E-J408 <786961252@qq.com>
2025-12-15 18:12:57 +0800

    完成ADMM-Net的整体架构部分

--- admm-net.py
+++ admm-net.py
@@ -1,3 +1,110 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
 
+class PhiLayer(nn.Module):
+    """Phi 对应层"""
+
+    def __init__(self, M, N):
+        super(PhiLayer, self).__init__()
+
+    def forward(self, x):
+        """前向传播"""
+        pass
+
+
+class HLayer(nn.Module):
+    """H 对应层"""
+
+    def __init__(self, M, N):
+        super(HLayer, self).__init__()
+
+    def forward(self, x):
+        """前向传播"""
+        pass
+
+
+class GLayer(nn.Module):
+    """G 对应层"""
+
+    def __init__(self, M, N):
+        super(GLayer, self).__init__()
+
+    def forward(self, x):
+        """前向传播"""
+        pass
+
+
+class ZLayer(nn.Module):
+    """Z 对应层"""
+
+    def __init__(self, M, N):
+        super(ZLayer, self).__init__()
+
+    def forward(self, x):
+        """前向传播"""
+        pass
+
+
+class PeakSearchLayer(nn.Module):
+    """峰值搜索层"""
+
+    def __init__(self, M, N, L):
+        super(PeakSearchLayer, self).__init__()
+
+    def forward(self, x):
+        """前向传播"""
+        pass
+
+
+class ADMMNet(nn.Module):
+    """完整的ADMM-Net模型"""
+
+    def __init__(self, M, N, L=3, num_layers=10):
+        super(ADMMNet, self).__init__()
+        self.num_layers = num_layers
+        self.M, self.N, self.L = M, N, L
+
+        # 初始化网络组件
+        self.phiLayers = nn.ModuleList([
+            PhiLayer(M, N) for i in range(num_layers)
+        ])
+        self.hLayers = nn.ModuleList([
+            HLayer(M, N) for i in range(num_layers)
+        ])
+        self.gLayers = nn.ModuleList([
+            GLayer(M, N) for i in range(num_layers)
+        ])
+        self.zLayers = nn.ModuleList([
+            ZLayer(M, N) for i in range(num_layers)
+        ])
+
+        self.peakSearchLayer = PeakSearchLayer(M, N, L)
+
+    def forward(self, y, b, sigma):
+        """
+
+        :param y: 观测向量
+        :param b: 解调符号
+        :param sigma: 噪声上限
+        """
+        batch_size = y.shape[0]
+        M = self.M
+        N = self.N
+        G = torch.zeros(batch_size, M * N + 1, M * N + 1)
+        Z = torch.zeros(batch_size, M * N + 1, M * N + 1)
+        phi = torch.zeros(batch_size, M * N)
+
+        for l in range(self.num_layers):
+            # 第k层前向传播
+            phi = self.phiLayers[l](y, b, phi, G, Z, l)
+            H = self.hLayers[l](phi, G, Z, sigma, l)
+            G = self.gLayers[l](phi, H, Z, l)
+            Z = self.zLayers[l](phi, H, G, Z, l)
+
+        # 谱峰搜索
+        tau_est, f_est = self.peakSearchLayer(phi, b)
+
+        return tau_est, f_est
